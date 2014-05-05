e32dbee00afe7c1cd43da41ce743f27d088a70dd
Paramjit Oberoi <paramjit@amazon.com>
2014-05-05 13:50:09 -0700

    C# version contributed by Vincent Jacquet

---
+++ languages/cs/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepanov.QuickSort
+{
+    /// <summary>
+    /// C# implementation of quick sort implementation from Stepanov conversations course.
+    /// Original code and data can be found at <https://github.com/psoberoi/stepanov-conversations-course>.
+    /// Conversations can be watched on <https://www.youtube.com/playlist?list=PLHxtyCq_WDLXFAEA-lYoRNQIezL_vaSX->.
+    /// </summary>
+    class Program
+    {
+        const int minSize = 8;
+        const int maxSize = 16 * 1024 * 1024;
+
+        readonly long[] _data;
+        readonly long[] _buffer;
+
+        Program() {
+            _data = new long[maxSize];
+            _buffer = new long[maxSize];
+            _data.Iota<long>().Suffle();
+        }
+
+         long TimeSort(int size) {
+             var stopwatch = Stopwatch.StartNew();
+
+             int first = 0;
+             while (first <= maxSize - size) {
+                 Array.Copy(_data, _buffer, size);
+                 _buffer.QuickSort(0, size);
+                 //Array.Sort(_buffer, 0, size);
+                 first += size;
+             }
+
+             return (long)(stopwatch.Elapsed.TotalMilliseconds * 10000000L);
+        }
+
+         void Run() {
+             Console.WriteLine("{0,-12} {1,6} {2,6}", "size", "time", "log2");
+
+             int lg = 3;
+             for (int arraySize = minSize; arraySize <= maxSize; arraySize *= 2) {
+                 long time = TimeSort(arraySize);
+                 Console.WriteLine("{0,-12} {1,6} {2,6:N2}", arraySize, time / maxSize, (double)time / (maxSize * lg));
+
+                 ++lg;
+             }
+         }
+
+        static void Main(string[] args) {
+            new Program().Run();
+        }
+    }
+
+    public static class ArrayExtensions
+    {
+        public static T[] Suffle<T>(this T[] array, int index, int count, Random random) {
+            for (int i = 1; i < count; ++i) {
+                int randomPlace = Math.Abs(random.Next()) % (i + 1);
+                T tmp = array[randomPlace];
+                array[randomPlace] = array[i + index];
+                array[i + index] = tmp;
+            }
+            return array;
+        }
+
+        public static T[] Suffle<T>(this T[] array) {
+            return Suffle(array, 0, array.Length, new Random());
+        }
+
+        public static long[] Iota<T>(this long[] array) {
+            for (int i = 0; i < array.Length; ++i) {
+                array[i] = (long)i;
+            }
+            return array;
+        }
+    }
+}
---
+++ languages/cs/QuickSort64.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepanov.QuickSort
+{
+    public static class QuickSort64
+    {
+        static void UnguardedLinearInsert(long[] array, int last, long value) {
+            int previous = last;
+            while (value < array[--previous])
+                array[last--] = array[previous];
+            array[last] = value;
+        }
+
+        static void UnguardedInsertionSort(long[] array, int first, int last) {
+            for (int i = first; i != last; ++i)
+                UnguardedLinearInsert(array, i, array[i]);
+        }
+
+        static void LinearInsert(long[] array, int first, int last, long value) {
+            if (value < array[first]) {
+                while (first != last--)
+                    array[last + 1] = array[last];
+                array[first] = value;
+            } else {
+                UnguardedLinearInsert(array, last, value);
+            }
+        }
+
+        static void InsertionSort(long[] array, int first, int last) {
+            if (first == last)
+                return;
+            for (int i = first + 1; i != last; ++i)
+                LinearInsert(array, first, i, array[i]);
+        }
+
+        public static long MedianOf3(long a, long b, long c) {
+            if (a < b) {
+                if (b < c)
+                    return b;
+                else if (a < c)
+                    return c;
+                else
+                    return a;
+            } else if (a < c)
+                return a;
+            else if (b < c)
+                return c;
+            else
+                return b;
+        }
+
+        static int UnguardedPartition(long[] array, int first, int last, long pivot) {
+            --last;
+            while (array[first] < pivot)
+                ++first;
+            while (pivot < array[last])
+                --last;
+            while (first < last) {
+                long tmp = array[first];
+                array[first] = array[last];
+                array[last] = tmp;
+                ++first;
+                --last;
+                while (array[first] < pivot)
+                    ++first;
+                while (pivot < array[last])
+                    --last;
+            }
+            return first;
+        }
+
+        static void QuickSortLoop(long[] array, int first, int last, int threshold) {
+            int len = last - first;
+            while (len > threshold) {
+                int middle = first + (last - first) / 2;
+                long pivot = MedianOf3(array[first], array[middle], array[last - 1]);
+                int cut = UnguardedPartition(array, first, last, pivot);
+                if (last - cut < cut - first) {
+                    QuickSortLoop(array, cut, last, threshold);
+                    last = cut;
+                } else {
+                    QuickSortLoop(array, first, cut, threshold);
+                    first = cut;
+                }
+                len = last - first;
+            }
+        }
+
+        public static void QuickSort(this long[] array, int first, int last) {
+            int len = last - first;
+            int threshold = 16;
+            if (len <= threshold) {
+                InsertionSort(array, first, last);
+            } else {
+                QuickSortLoop(array, first, last, threshold);
+                int middle = first + threshold;
+                InsertionSort(array, first, middle);
+                UnguardedInsertionSort(array, middle, last);
+            }
+        }
+    }
+}
